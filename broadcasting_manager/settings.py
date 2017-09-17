7a87107e03316c1607848973db2302165b43ecaf
Đỗ Ngọc Ngoạn <ngocngoan060288@gmail.com>
2017-09-17 11:19:08 +0700

    initialize the apps

--- broadcasting_manager/settings.py
+++ broadcasting_manager/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'areas.apps.AreasConfig',
-    'basestaions.apps.BasestaionsConfig',
+    'basestations.apps.BasestationsConfig',
     'broadcastingstatus.apps.BroadcastingstatusConfig',
     'broadcasts.apps.BroadcastsConfig',
     'contracts.apps.ContractsConfig',
